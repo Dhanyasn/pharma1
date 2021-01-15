ccd48401eb8cee1aa547f33baa41cbd03cb6628d
Dhanyasn <60551972+Dhanyasn@users.noreply.github.com>
2021-01-15 15:32:33 +0530

    scripts commit

---
+++ scripts/database.js
@@ -0,0 +1,12 @@
+ 
+const express =require("express")
+
+const connect={host: "localhost",
+  user: "root",
+  password: "dkcknsani",
+  database:"todoapp"}
+
+module.exports={
+	
+	connect:connect
+};
